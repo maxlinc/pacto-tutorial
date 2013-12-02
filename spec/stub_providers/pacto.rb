6ef6ec31f178af49426d5067bc3cb4acfd896848
Max Lincoln <max@devopsy.com>
2013-12-02 00:23:41 -0300

    Add pacto stub provider

---
+++ spec/stub_providers/pacto.rb
@@ -0,0 +1,6 @@
+require 'pacto'
+hosts = Dir["#{Pacto.configuration.contracts_path}/*"].each do |host|
+  host = File.basename host
+  Pacto.load_all host, "https://#{host}", :default
+end
+Pacto.use :default
