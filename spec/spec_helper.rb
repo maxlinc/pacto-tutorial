bfb3ea391d5682fc2f4df868a3970b3610950208
Max Lincoln <max@devopsy.com>
2013-12-02 00:23:05 -0300

    stub is more appropriate then mock (see article on mocks aren't stubs)

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -8,9 +8,9 @@ end
 pacto_mode = ENV['PACTO_MODE']
 require "pacto_modes/#{pacto_mode}" if pacto_mode
 
-mock_provider = ENV['MOCK_WITH']
-if mock_provider
-  require "mock_providers/#{mock_provider}"
+stub_provider = ENV['STUB_WITH']
+if stub_provider
+  require "stub_providers/#{stub_provider}"
 else
   # We're live!
   WebMock.allow_net_connect!
