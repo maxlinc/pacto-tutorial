e320b4217648d074a48a635f62d6c7c1cce027d7
Max Lincoln <max@devopsy.com>
2013-12-04 02:31:11 -0300

    Add validations

--- spec/external_request_spec.rb
+++ spec/external_request_spec.rb
@@ -7,5 +7,9 @@ describe 'External request', :vcr => true do
     response = Net::HTTP.get(uri)
 
     expect(response).to be_an_instance_of(String)
+    expect(Pacto).to have_validated(:get, 'https://api.github.com/repos/thoughtbot/factory_girl/contributors')
+        .against_contract /contributors.json/
+    expect(Pacto).to_not have_failed_validations
+    expect(Pacto).to_not have_unmatched_requests
   end
 end
